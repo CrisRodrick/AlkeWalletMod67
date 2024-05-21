dadea757ec5c6e10f3bb8fedc6a1629166f40f5f
CrisRodrick <ctnrodriguez@gmail.com>
2024-05-20 21:11:25 -0400

    version 12 : traspaso de codigo a fragments antes del delete de actividades

--- app/src/main/java/com/example/alkewalletandroid/View/MainActivity2.kt
+++ app/src/main/java/com/example/alkewalletandroid/View/MainActivity2.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.example.alkewalletandroid.ViewModel.ViewModel2
+import com.example.alkewalletandroid.Login_singup.ViewModel_login_singup
 import com.example.alkewalletandroid.databinding.ActivityMain2Binding
 import androidx.activity.viewModels
 
@@ -13,7 +13,7 @@ import androidx.activity.viewModels
 
 class MainActivity2 : AppCompatActivity() {
 
-    private val viewModel2 by viewModels<ViewModel2>()
+    private val viewModelLoginsingup by viewModels<ViewModel_login_singup>()
     private lateinit var binding: ActivityMain2Binding
 
 
@@ -23,17 +23,17 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(binding.root)
 
         // Observando el LiveData en el ViewModel2
-        viewModel2.actividadDestino.observe(this, Observer { actividad ->
+        viewModelLoginsingup.actividadDestino.observe(this, Observer { actividad ->
             val intent = Intent(this, actividad)
             startActivity(intent)
         })
 
         binding.button1.setOnClickListener {
-            viewModel2.determinarActividadSiguiente(1)
+            viewModelLoginsingup.determinarActividadSiguiente(1)
         }
 
         binding.txt2.setOnClickListener {
-            viewModel2.determinarActividadSiguiente(2)
+            viewModelLoginsingup.determinarActividadSiguiente(2)
         }
     }
 }
